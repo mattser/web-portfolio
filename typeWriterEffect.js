c0670750033840549ba21c76c7d2dc79b7ee70c1
mattser <jomatser@gmail.com>
2022-01-19 09:14:28 +0000

    added javascript animation

--- typeWriterEffect.js
+++ typeWriterEffect.js
@@ -1,9 +1,11 @@
 const text = "hello world! ";
-const speed = 750;
+const speed = 200;
+let i = 0;
 
-function typeWriter {
-  for (let i = 0; i < text.length; i++) {
+function typeWriter() {
+  if (i < text.length) {
     document.getElementById("typed").innerHTML += text.charAt(i);
     setTimeout(typeWriter,speed);
+    i++;
   }
 }
