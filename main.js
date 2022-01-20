86060ad40c13a1c373f0a3a7aceb32f4710718da
mattser <jomatser@gmail.com>
2022-01-20 14:37:37 +0000

    added link arrows and smooth scrolling

--- main.js
+++ main.js
@@ -5,8 +5,7 @@ let i = 0;
 const typeWriter = () => {
   let currentText = document.getElementById("typed").innerHTML;
   if (i < text.length) {
-    // currentText = currentText.slice(0,currentText.length-1)
-    currentText += text.charAt(i); //+ "|";
+    currentText += text.charAt(i);
     i++;
     document.getElementById("typed").innerHTML = currentText;
     setTimeout(typeWriter,speed);
